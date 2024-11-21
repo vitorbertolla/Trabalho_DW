b07500ba4b54415fb4c9680dcf4d367af92e2ce2
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-11-21 11:33:23 -0300

    .

---
+++ README.md
@@ -0,0 +1,11 @@
+**To-Do-List**
+Esse repositório serve para fazer a tarefa da aula de  Desenvolvimento Web 
+
+Utilizando as linguagens e tecnologias:
+- Html
+- Css
+- JavaScript
+
+Feito por:
+- Hiago Gimenez
+- Vitor Bertolla
---
+++ script.js
@@ -0,0 +1,27 @@
+const taks = {
+    lista: [
+        {
+            descricao: "teste",
+            check: false
+        }
+    ]
+
+}
+
+let 
+
+
+function crairTaks(descricao) {
+    taks.lista.push({descricao: descricao, check: false})
+    let Taks = document.createAttribute('li')
+    Taks.style.height = "10vh"
+    Taks.style.background = "red"
+}
+
+function atualizarTaks() {
+    
+}
+
+function deltarTaks() {
+    
+}
