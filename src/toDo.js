8839d3fa264240902bf90c5a5f69038edb26d362
Vitor Hugo Almeida Bertolla <163414035+vitorbertolla@users.noreply.github.com>
2024-11-22 15:32:39 -0300

    .

--- src/toDo.js
+++ src/toDo.js
@@ -6,7 +6,7 @@ let descricao = document.querySelector("#taksTexto")
 let btn = document.querySelector("#buttonCriar")
 let localTaks = document.querySelector('ul')
 
-btn.addEventListener("click", () => criarTask(descricao.value));
+btn.addEventListener("click", () => criarTask(descricao.value))
 
 function criarTask(descricao){
     if (descricao == "") {
@@ -27,18 +27,50 @@ function criarTask(descricao){
     checkbox.checked = false
     caixaTaks.appendChild(checkbox)
 
-    let deleteTask = document.createElement('button')
+    let deleteTask = document.createElement('img')
     deleteTask.src = "https://cdn-icons-png.flaticon.com/512/1345/1345874.png"
     caixaTaks.appendChild(deleteTask)
     deleteTask.classList.add('deleteTask')
 
     deleteTask.addEventListener('click', () =>{
-        delete tarefas[pos]
-    })
+        tarefas.splice(pos, 1)
+        localTaks.removeChild(caixaTaks) 
+            })
+
+
+    let updateTask = document.createElement('img')
+    updateTask.src ="https://cdn-icons-png.flaticon.com/512/16/16310.png"
+    caixaTaks.appendChild(updateTask)
+    updateTask.classList.add("uptadeTask")
+    updateTask.addEventListener("click", () => {
+        let inputUpdate = document.createElement('input')
+        inputUpdate.classList.add("inputUpdate")
+        let caixaUpdate = document.createElement('div')
+        caixaUpdate.classList.add("caixaUpdate")
+        caixaUpdate.appendChild(inputUpdate)
+
+        tarefas[pos].nome = inputUpdate.value
+        let btnConfirm = document.createElement('button')
+        btnConfirm.textContent = "Salvar"
+        btnConfirm.classList.add("btnConfirm")
+        caixaUpdate.appendChild(btnConfirm)
+
+        caixaTaks.appendChild(caixaUpdate)
+
+        btnConfirm.addEventListener("click", () => {
+            tarefas[pos].nome = inputUpdate.value
+
+            p.textContent = inputUpdate.value
+
+            caixaTaks.removeChild(caixaUpdate)
+            })
 
     pos++
+
+        }
+    }
 }
-}
+
 
 
 // console.log(tarefas)
