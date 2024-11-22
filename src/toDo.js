b4b719144615036e3122e5b50817483af7e743c8
Vitor Hugo Almeida Bertolla <163414035+vitorbertolla@users.noreply.github.com>
2024-11-22 15:00:35 -0300

    .

--- src/toDo.js
+++ src/toDo.js
@@ -1,15 +1,66 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const criarTask = (descricao) => {
-        let localTaks = document.querySelectorAll('ul')
+let tarefas = []
 
-        let caixaTaks = document.createElement('li')
-        caixaTaks.classList.add('caixaTaks')
+let pos = 0 
 
-        let checkbox = document.createElement('input')
-        checkbox.setAttribute("type", "checkbox")
-        checkbox.checked = false
+let descricao = document.querySelector("#taksTexto")
+let btn = document.querySelector("#buttonCriar")
+let localTaks = document.querySelector('ul')
 
-        let p = document.createElement('p')
-        p.textContent = descricao
-    }
-})
+btn.addEventListener("click", () => criarTask(descricao.value));
+
+function criarTask(descricao){
+    if (descricao == "") {
+        return window.alert('digite uma Tarefa para adicionar')
+    } else{
+    tarefas.push({id:pos, nome:descricao, feito:false})
+
+    let caixaTaks = document.createElement('li')
+    caixaTaks.classList.add('caixaTaks')
+    localTaks.appendChild(caixaTaks)
+
+    let p = document.createElement('p')
+    caixaTaks.appendChild(p)
+    p.textContent = descricao
+
+    let checkbox = document.createElement('input')
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.checked = false
+    caixaTaks.appendChild(checkbox)
+
+    let deleteTask = document.createElement('button')
+    deleteTask.src = "https://cdn-icons-png.flaticon.com/512/1345/1345874.png"
+    caixaTaks.appendChild(deleteTask)
+    deleteTask.classList.add('deleteTask')
+
+    deleteTask.addEventListener('click', () =>{
+        delete tarefas[pos]
+    })
+
+    pos++
+}
+}
+
+
+// console.log(tarefas)
+
+
+
+
+
+
+
+// const criarTask = (descricao) => {
+//     
+
+
+
+   
+
+//     
+
+//     caixaTaks.appendChild(checkbox)
+//     caixaTaks.appendChild(p)
+// }
+// let tarefas = [
+//     {idTarefa: 1, nome: "fazer trabalho", feito:false}
+// ]
